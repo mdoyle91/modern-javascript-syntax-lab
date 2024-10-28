ee49af236959645487554fb7ff7e90c7386b3d89
mdoyle91 <mike.w.doyle91@gmail.com>
2024-10-28 10:49:03 -0400

    Exercise 8 Completed

--- exercises.js
+++ exercises.js
@@ -73,3 +73,21 @@
 // };
 
 // console.log(userProfile);
+
+// Create a function that takes two parameters, `noun` and `adjective`, both with the following respective default values:
+
+// 1. `cat`
+
+// 2. `white`
+
+// The function should log a sentence 'The cat is white.' by default. The function should substitute the appropriate parameters when supplied arguments.
+
+// Your code here
+
+// function sentenceCompletion(noun = `cat`, adjective = `white`) {
+//   console.log(`The ${noun} is ${adjective}`);
+// }
+
+// sentenceCompletion();
+
+// sentenceCompletion(`dog`, `black`);
